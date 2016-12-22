0eee2626b4c1248ed1d29cc11f48e6a8ded91ea0
Igor Alimov <alimov@corp.sputnik.ru>
2016-12-22 12:57:35 +0300

    Вариант редактирования многие ко многим StackedInline

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,9 +6,13 @@ from .models import Author, Book
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'middle_name', 'born')
 
+
+class AuthorInline(admin.StackedInline):
+    model = Book.authors.through
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'year', 'isbn')
-    filter_vertical=('authors',)
+    inline = (AuthorInline,)
 
 
 admin.site.register(Book, BookAdmin)
